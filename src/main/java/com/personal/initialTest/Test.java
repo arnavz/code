b7903d7c5c53bbbd34f487a458116732da38a7a6
Arnav Bhattacharya <YATRAONLINE\arnav.bhattacharya@lap0002965.yatraonline.local>
2020-07-19 15:38:37 +0530

    TestCommit#2

---
+++ src/main/java/com/personal/initialTest/Test.java
@@ -0,0 +1,12 @@
+package com.personal.initialTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Initial Commit from Linux for two parallel git accounts!!");
+
+	}
+
+}
